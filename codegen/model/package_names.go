b752a82a0d6a98a2fc4f9f643c4ed1ca430aded1
Scott Weiss <sdw35@cornell.edu>
2019-11-13 16:38:48 -0500

    add package names vars

---
+++ codegen/model/package_names.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"fmt"
+	"github.com/gertd/go-pluralize"
+	"strings"
+)
+
+var (
+	// function for determining the relative path of generated api types package
+	TypesRelativePath = func(kind, version string) string {
+		c := pluralize.NewClient()
+		return fmt.Sprintf("pkg/apis/%v/%v", strings.ToLower(c.Plural(kind)), version)
+	}
+
+	// function for determining the relative path of generated schduler package
+	SchedulerRelativePath = "pkg/scheduler"
+
+	// function for determining the relative path of generated finalizer package
+	FinalizerRelativePath = "pkg/finalizer"
+
+	// function for determining the relative path of generated parameters package
+	ParametersRelativePath = "pkg/parameters"
+
+	// function for determining the relative path of generated metrics package
+	MetricsRelativePath = "pkg/metrics"
+)
