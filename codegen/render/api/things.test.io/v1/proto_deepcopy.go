a652130f4b5b3fa952c6ca2272ef30d15b4110c0
Scott Weiss <sdw35@cornell.edu>
2020-01-07 14:54:30 -0500

    regen

---
+++ codegen/render/api/things.test.io/v1/proto_deepcopy.go
@@ -0,0 +1,33 @@
+// This file contains generated Deepcopy methods for
+// Spec and Status protobuf types
+
+package v1
+
+import (
+	fmt "fmt"
+
+	proto "github.com/gogo/protobuf/proto"
+
+	math "math"
+
+	_ "github.com/gogo/protobuf/gogoproto"
+
+	_ "github.com/gogo/protobuf/types"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ = proto.Marshal
+var _ = fmt.Errorf
+var _ = math.Inf
+
+// DeepCopyInto supports using AttributeManifest within kubernetes types, where deepcopy-gen is used.
+func (in *PaintSpec) DeepCopyInto(out *PaintSpec) {
+	p := proto.Clone(in).(*PaintSpec)
+	*out = *p
+}
+
+// DeepCopyInto supports using AttributeManifest within kubernetes types, where deepcopy-gen is used.
+func (in *TubeStatus) DeepCopyInto(out *TubeStatus) {
+	p := proto.Clone(in).(*TubeStatus)
+	*out = *p
+}
